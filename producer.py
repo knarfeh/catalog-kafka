fa32bae3d0ac8f78fb3e67d30bea0d2b73d9ea42
knarfeh <junh@alauda.io>
2016-12-06 14:30:56 +0800

    Add use case

---
+++ producer.py
@@ -0,0 +1,23 @@
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+# Asynchronous by default
+future = producer.send('my-topic', b'raw_bytes')
+
+# Block for 'synchronous' sends
+try:
+    record_metadata = future.get(timeout=10)
+except KafkaError:
+    # Decide what to do if produce request failed...
+    log.exception()
+    pass
+
+# Successful result returns assigned partition and offset
+print (record_metadata.topic)
+print (record_metadata.partition)
+print (record_metadata.offset)
+
+# produce keyed messages to enable hashed partitioning
+producer.send('my-topic', key=b'foo', value=b'bar')
